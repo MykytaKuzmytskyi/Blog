7b81f5336c8de50c479ee5e9f0467f4a35d89b4d
Mykyta Kuzmytskyi <nykytakuzmytskyi@gmail.com>
2023-06-23 01:30:26 +0300

    Added UserUpdateForm.

--- blog/forms.py
+++ blog/forms.py
@@ -1,7 +1,11 @@
+import os
+
 from captcha.fields import ReCaptchaField
 from captcha.widgets import ReCaptchaV2Checkbox
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
+from django.db.models.signals import pre_save
+from django.dispatch import receiver
 
 from .models import Comment, Post, User
 from mptt.forms import TreeNodeChoiceField
@@ -18,6 +22,35 @@ class UserCreateForm(UserCreationForm):
             "last_name",
             "avatar",
         )
+
+
+class UserUpdateForm(forms.ModelForm):
+
+    class Meta:
+        model = User
+        fields = [
+            "email",
+            "first_name",
+            "last_name",
+            "avatar",
+        ]
+
+    @receiver(pre_save)
+    def pre_save_image(sender, instance, *args, **kwargs):
+        """ instance old image file will delete from os """
+        try:
+            old_avatar = instance.__class__.objects.get(
+                id=instance.id
+            ).avatar.path
+            try:
+                new_avatar = instance.image.path
+            except:
+                new_avatar = None
+            if new_avatar != old_avatar:
+                if os.path.exists(old_avatar):
+                    os.remove(old_avatar)
+        except:
+            pass
 
 
 class PostCreateForm(forms.ModelForm):
