a3c9cd846be4b58f5e8f845ef75648c41d94a132
Mykyta Kuzmytskyi <nykytakuzmytskyi@gmail.com>
2023-06-22 18:44:02 +0300

    Added debug_toolbar.

--- social_blog/settings.py
+++ social_blog/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     "crispy_bootstrap4",
+    "debug_toolbar",
     "crispy_forms",
     "captcha",
     'blog',
@@ -51,6 +52,7 @@ CRISPY_TEMPLATE_PACK = "bootstrap4"
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    "debug_toolbar.middleware.DebugToolbarMiddleware",
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
--- social_blog/urls.py
+++ social_blog/urls.py
@@ -23,4 +23,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('blog.urls', namespace='blog')),
     path("accounts/", include("django.contrib.auth.urls")),
+    path("__debug__/", include("debug_toolbar.urls")),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
